80acbc89f32fee1cd757b5f80a0b3410489fae96
Alexandra Hansma <ahansma@butler.edu>
2017-05-17 15:14:29 -0400

    Create a student object with a name and id

--- js/app.js
+++ js/app.js
@@ -4,17 +4,28 @@ const megaroster =
 {
     init()
     {
+        this.max = 0;
         //adds event listener to form
         document
             .querySelector('#new-student')
-            .addEventListener('submit', this.addStudent)
+            .addEventListener('submit', this.addStudent.bind(this))
     },
 
     addStudent(ev)
     {
         event.preventDefault()
-        const studentName = ev.target.studentName.value
-        console.log(studentName)
+        const f = ev.target
+        const student = {
+            id: this.max + 1,
+            name: f.studentName.value,
+        }
+        this.buildListItem(studentName)
+        this.max ++
+    },
+
+    buildListItem(student)
+    {
+        console.log(student)
     },
 }
 megaroster.init()
