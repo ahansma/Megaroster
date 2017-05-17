6d38ff7c7156ef07bcd596abc700402257702e24
Alexandra Hansma <ahansma@butler.edu>
2017-05-17 15:28:17 -0400

    Add students to an array

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 $(document).foundation()
 
 const megaroster = {
+  students: [],
+
   init() {
     this.studentList = document.querySelector('#student-list')
     this.max = 0
-    //add event listener
     document
       .querySelector('#new-student')
       .addEventListener('submit', this.addStudent.bind(this))
@@ -17,10 +18,12 @@ const megaroster = {
       id: this.max + 1,
       name: f.studentName.value,
     }
+
+    this.students.push(student)
     
-    const listItem  = this.buildListItem(student)
+    const listItem = this.buildListItem(student)
     this.studentList.appendChild(listItem)
-   
+    
     this.max ++
   },
 
