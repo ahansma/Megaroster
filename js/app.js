3a412a1428dd29768f88619a3ae59d05db321ea3
Alexandra Hansma <ahansma@butler.edu>
2017-05-17 14:46:18 -0400

    Setup submit handler  for the new student form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 $(document).foundation()
+
+const megaroster = 
+{
+    init()
+    {
+        //adds event listener to form
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudent)
+    },
+
+    addStudent(ev)
+    {
+        event.preventDefault()
+        const studentName = ev.target.studentName.value
+        console.log(studentName)
+    },
+}
+megaroster.init()
