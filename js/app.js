e6615ad43113dfedcf2f23751d4667e18e472fb1
Alexandra Hansma <ahansma@butler.edu>
2017-05-18 10:34:27 -0400

    Remove students from the ul, but not the array

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@ const megaroster = {
   init() {
     this.studentList = document.querySelector('#student-list')
     this.max = 0
+    this.setupEventListeners()
+  },
+
+  setupEventListeners() {
     document
       .querySelector('#new-student')
       .addEventListener('submit', this.addStudent.bind(this))
@@ -21,23 +25,41 @@ const megaroster = {
 
     this.students.unshift(student) //use push instead of unshift to put the list item after instead of before in the array
     
-    const listItem = this.buildListItem(student)
+     const listItem = this.buildListItem(student)
     this.prependChild(this.studentList, listItem)
     //this.studentList.appendChild(listItem) --> to put the next list item after instead of before
     this.max ++
     f.reset()
   },
 
-  prependChild(parent, child)
-  {
-    parent.insertBefore(child, this.studentList.firstChild)
+  prependChild(parent, child) {
+    parent.insertBefore(child, parent.firstChild)
   },
 
   buildListItem(student) {
-    const li = document.createElement('li')
-    li.textContent = student.name
-    li.dataset.id =  student.id
+    const template = document.querySelector('.student.template')
+    const li = template.cloneNode(true)
+    li.querySelector('.student-name').textContent = student.name
+    li.dataset.id = student.id
+    this.removeClassName(li, 'template')
+
+    li
+      .querySelector('button.remove')
+      .addEventListener('click', this.removeStudent.bind(this))
+
     return li
   },
+
+  removeStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').remove()
+
+    // Remove it from the this.students array
+    // this.students.splice(?, 1)
+  },
+
+  removeClassName(el, className) {
+    el.className = el.className.replace(className, '').trim()
+  }
 }
 megaroster.init()
